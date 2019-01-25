73b68df1205e5c6dbcb782ecc80788c2186f498f
Mellila Bouam <em_bouam@esi.dz>
2019-01-25 11:53:43 +0100

    en cours de parallélisation

--- ijoux/test_v0.c
+++ ijoux/test_v0.c
@@ -44,28 +44,35 @@ void v_0(int u, int v)
 	// B : charger les tranches j * v --> (j+1)*v [exclu]
 	// C : charger les tranches (i ^ j) * v --> ((i ^ j)+1)*v [exclu]
 
-        u32 idx[3] = {i, j, i ^ j};
-
 	// A & B
         char filename[255];
 	MPI_Comm comm_I, comm_J, comm_IJ;
 	MPI_Comm_split(MPI_COMM_WORLD, i, 0, &comm_I);
 	MPI_Comm_split(MPI_COMM_WORLD, j, 0, &comm_J);
 	MPI_Comm_split(MPI_COMM_WORLD, i ^ j, 0, &comm_IJ);
-	for (u32 k = 0;  k < 2; k++) {
-                char *kind_name[2] = {"foo", "bar"};
-                for (u32 r = 0; r < v; r++) {
-			sprintf(filename, "%s/%s.%03x", hash_dir, kind_name[k], idx[k] * v + r);
-			printf("Je charge %s\n",filename);
-	                all_tasks[r].L[k] = load_file(filename, &all_tasks[r].n[k], MPI_COMM_WORLD);
-	                all_tasks[r].n[k] /= 8;
-		}
+
+	// A
+        for (u32 r = 0; r < v; r++) {
+			sprintf(filename, "%s/foo.%03x", hash_dir, i * v + r);
+			printf("Je charge %s\n", filename);
+	                all_tasks[r].L[0] = load_file(filename, &all_tasks[r].n[0], comm_I);
+	                all_tasks[r].n[0] /= 8;
+	}
+
+	// B
+ 	for (u32 r = 0; r < v; r++) {
+                        sprintf(filename, "%s/bar.%03x", hash_dir, j * v + r);
+                        printf("Je charge %s\n", filename);
+                        all_tasks[r].L[1] = load_file(filename, &all_tasks[r].n[1], comm_J);
+                        all_tasks[r].n[1] /= 8;
         }
+
+
 	// C
         for (u32 r = 0; r < v; r++) {
-        	sprintf(filename, "%s/%03x", slice_dir, idx[2] * v + r);
+        	sprintf(filename, "%s/%03x", slice_dir, (i ^ j) * v + r);
 		printf("Je charge %s\n", filename);
-	        all_tasks[r].slices = load_file(filename, &all_tasks[r].slices_size, MPI_COMM_WORLD);
+	        all_tasks[r].slices = load_file(filename, &all_tasks[r].slices_size, comm_IJ);
 	}
 
 	for (u32 r = 0; r < v; r++)
@@ -78,6 +85,7 @@ void v_0(int u, int v)
         		}
 
 	// GO !
+	struct task_result_t *all_solutions = result_init();
         for (u32 r = 0; r < v; r++)
 	        for (u32 s = 0; s < v; s++) {
 			// fabrique la "tâche"
@@ -93,19 +101,31 @@ void v_0(int u, int v)
 			struct task_result_t *solutions = iterated_joux_task_v3(&task);
 
                         printf("%d solutions\n", solutions->size);
-                        for (u32 u = 0; u < solutions->size; u++)
+                        for (u32 u = 0; u < solutions->size; u++) {
                                 printf("%016" PRIx64 " ^ %016" PRIx64 " ^ %016" PRIx64 " == 0\n",
                                                 solutions->solutions[u].x,
                                                 solutions->solutions[u].y,
                                                 solutions->solutions[u].z);
+				result_report_solution(all_solutions,
+						solutions->solutions[u].x,
+                                                solutions->solutions[u].y,
+                                                solutions->solutions[u].z);
+			}
                         result_free(solutions);
 		}
+
+	// MPI_Gather sur un tableau de taille 1 : all_solutions->size;  [1 x MPI_UINT32_T]
+	// MPI_Gatherv sur un tableau de taille 3 * all_solutions->size : all_solutions->solutions  [MPI_UINT64_T]
+
+	// si je suis de rang zéro, je réaffiche tout. et j'enregistre dans un fichier !
+
 	printf("J'ai fini\n");
 	for (u32 r = 0; r < v; r++) {
 		free(all_tasks[r].L[0]);
 		free(all_tasks[r].L[1]);
 		free(all_tasks[r].slices);
 	}
+	result_free(all_solutions);
 }
 
 
