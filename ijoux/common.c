a7ac1ab137613a1ba62b9b33874db3013f2ab55c
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-02-18 15:26:27 +0100

    nettoyage du nettoyage

--- ijoux/common.c
+++ ijoux/common.c
@@ -12,105 +12,90 @@
 
 double wtime()
 {
-        return PAPI_get_real_usec() / 1e6;
+	return PAPI_get_real_usec() / 1e6;
 }
 
 u64 cycles()
 {
-        return PAPI_get_real_cyc();
+	return PAPI_get_real_cyc();
 }
 
 bool big_endian()
 {
-        return (htonl(0x47) == 0x47);
+	return (htonl(0x47) == 0x47);
 }
 
-void * aligned_alloc(size_t alignment, size_t size)
+void *aligned_alloc(size_t alignment, size_t size)
 {
-        void *p;
-        if (posix_memalign(&p, alignment, size) != 0)
-                return NULL;
-        return p;
+	void *p;
+	if (posix_memalign(&p, alignment, size) != 0)
+		return NULL;
+	return p;
 }
 
-void * load_file(char *filename, u64 *size_, MPI_Comm comm)
+void *load_file_MPI(const char *filename, u64 * size_, MPI_Comm comm)
 {
-  /* open the file (collective operation) */
-  MPI_File fh;
-  int rc = MPI_File_open(comm, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
-  if (rc != MPI_SUCCESS)
-    errx(1, "error opening %s", filename);
+	/* open the file (collective operation) */
+	MPI_File fh;
+	int rc = MPI_File_open(comm, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &fh);
+	if (rc != MPI_SUCCESS)
+		errx(1, "error opening %s", filename);
 
-  /* obtain file size */
-  MPI_Offset size;
-  MPI_File_get_size(fh, &size);
-  assert ((size % 8) == 0);
+	/* obtain file size */
+	MPI_Offset size;
+	MPI_File_get_size(fh, &size);
+	assert((size % 8) == 0);
 
-  /* allocate memory */
-  u64 *content = aligned_alloc(64, size);
-  if (content == NULL)
-    err(1, "failed to allocate memory");
+	/* allocate memory */
+	u64 *content = aligned_alloc(64, size);
+	if (content == NULL)
+		err(1, "failed to allocate memory");
 
-  /* read (collective) */
-  MPI_File_read_all(fh, content, size, MPI_BYTE, MPI_STATUS_IGNORE);
+	/* read (collective) */
+	MPI_File_read_all(fh, content, size, MPI_BYTE, MPI_STATUS_IGNORE);
 
-  /* we're done */
-  MPI_File_close(&fh);
-  *size_ = size;
+	/* we're done */
+	MPI_File_close(&fh);
+	*size_ = size;
 
-  /* byte-swap if necessary */
-  if (big_endian()) {
+	/* byte-swap if necessary */
+	if (big_endian()) {
                 #pragma omp parallel for
-                for (u32 i = 0; i < size / 8; i++)
-                        content[i] = bswap_64(content[i]);
-        }
-
-        return content;
+		for (u32 i = 0; i < size / 8; i++)
+			content[i] = bswap_64(content[i]);
+	}
+	return content;
 }
 
-
-
-struct task_result_t * result_init()
+struct task_result_t *result_init()
 {
-        struct task_result_t *result = malloc(sizeof(*result));
-        if (result == NULL)
-                err(1, "cannot allocate task result object");
-        result->size = 0;
-        result->capacity = 128;
-        result->solutions = malloc(result->capacity * sizeof(struct solution_t));
-        return result;
+	struct task_result_t *result = malloc(sizeof(*result));
+	if (result == NULL)
+		err(1, "cannot allocate task result object");
+	result->size = 0;
+	result->capacity = 128;
+	result->solutions =
+	    malloc(result->capacity * sizeof(struct solution_t));
+	return result;
 }
 
 void result_free(struct task_result_t *result)
 {
-        free(result->solutions);
-        free(result);
+	free(result->solutions);
+	free(result);
 }
 
-void report_solution(struct task_result_t *result,struct solution_t solution)
+void report_solution(struct task_result_t *result, const struct solution_t *sol)
 {
-
-    u64 x = solution.solution[0];
-    u64 y = solution.solution[1];
-    u64 z = solution.solution[2]; 	
-    if ((x ^ y ^ z) != 0)
-        warnx("Fake solution reported");
-    if (result->size == result->capacity) {
-        result->solutions = realloc(result->solutions, 2 * result->capacity);
-        if (result->solutions == NULL)
-            err(1, "failed to re-alloc solutions array");
-        result->capacity *= 2;
-    }
-    result->solutions[result->size].solution[0] = x;
-    result->solutions[result->size].solution[1] = y;
-    result->solutions[result->size].solution[2] = z;
-    result->solutions[result->size].task_index[0] = solution.task_index[0];
-    result->solutions[result->size].task_index[1] = solution.task_index[1];
-    result->size++;
+	if ((sol->val[0] ^ sol->val[1] ^ sol->val[2]) != 0)
+		warnx("Fake solution reported");
+	if (result->size == result->capacity) {
+		result->solutions =
+		    realloc(result->solutions, 2 * result->capacity);
+		if (result->solutions == NULL)
+			err(1, "failed to re-alloc solutions array");
+		result->capacity *= 2;
+	}
+	result->solutions[result->size] = *sol;	          // struct copy
+	result->size++;
 }
-
-
-
-
-
-
--- ijoux/post_processing.c
+++ ijoux/post_processing.c
@@ -7,104 +7,95 @@
 #include <glob.h>
 #include <getopt.h>
 #include <math.h>
+
 #include "common.h"
 #include "../preprocessing/sha256.h"
 #include "../preprocessing/hasher.h"
 
-typedef uint64_t u64;
-typedef uint32_t u32;
-
 //char * hash_di = "/home/mellila/foobar/data/hashes";
 //char * slice_di = "/home/mellila/foobar/data/slice";
 const char * dict_dir = "/home/mellila/foobar/data/dict";
+
 bool compute_full_hash(int kind, struct preimage_t *preimage, u32 *hash);
 
 void check_solutions(char *solutions_filename, u32 k)
 {
-		
-		//Charger le fichier de solutions 
-		struct stat infos;
-     		if (stat(solutions_filename, &infos))
-                	err(1, "fstat failed on %s", solutions_filename);
-        	u64 size = infos.st_size;
-		u64 nb_solutions = size / sizeof(struct solution_t);			
-		struct solution_t *solutions = malloc( size);
-		FILE *f = fopen(solutions_filename, "r");
-        	if (f == NULL)
-        		err(1, "fopen failed (%s)", solutions_filename);
-		u32 check = fread(solutions, sizeof(struct solution_t), nb_solutions, f);
-        	if (check != nb_solutions)
-                	errx(1, "incomplete read %s", solutions_filename);
-        	fclose(f);
+	// Charger le fichier de solutions 
+	struct stat infos;
+     	if (stat(solutions_filename, &infos))
+               	err(1, "fstat failed on %s", solutions_filename);
+        u64 size = infos.st_size;
+	u64 nb_solutions = size / sizeof(struct solution_t);			
+	struct solution_t *solutions = malloc( size);
+	FILE *f = fopen(solutions_filename, "r");
+        if (f == NULL)
+        	err(1, "fopen failed (%s)", solutions_filename);
+	u32 check = fread(solutions, sizeof(struct solution_t), nb_solutions, f);
+        if (check != nb_solutions)
+               	errx(1, "incomplete read %s", solutions_filename);
+        fclose(f);
 
-		/* Charles est un gros débile, il a oublié que turing était big-endian */
-		if (! (big_endian())) {
-        		#pragma omp parallel for
-                	for (u32 i = 0; i < nb_solutions; i++) 
-				for (u32 j = 0; j < 3; j++) 
-		                	solutions[i].solution[j] = bswap_64(solutions[i].solution[j]);
-				
-        	}
+	/* turing est big-endian */
+	if (!big_endian())
+               	for (u32 i = 0; i < nb_solutions; i++) 
+			for (u32 j = 0; j < 3; j++) 
+	                	solutions[i].val[j] = bswap_64(solutions[i].val[j]);
 
-		struct preimage_t preimages[nb_solutions][3];
-                for (u32 i = 0; i < nb_solutions; i++) {
-                        for (u32 j = 0; j < 3; j++) {
-                                preimages[i][j].counter = 0;
-                                preimages[i][j].nonce = 0;
+	struct preimage_t preimages[nb_solutions][3];
+	for (u32 i = 0; i < nb_solutions; i++) {
+                for (u32 j = 0; j < 3; j++) {
+                        preimages[i][j].counter = 0;
+                        preimages[i][j].nonce = 0;
+		}
+	}
+
+	// Charger les dictionnaires
+	int nb_preimages = 0;
+	for (u32 i = 0; i < (1 << k); i++) {
+		for (u32 kind = 0; kind < 3; kind++) {
+	                char pattern[255];
+	                char *kind_prefix[3] = {"foo", "bar", "foobar"};
+	                sprintf(pattern, "%s/%03x/%s.*.sorted", dict_dir, i, kind_prefix[kind]);
+	                glob_t globbuf;
+	                if (glob(pattern, 0, NULL, &globbuf))
+	                        err(1, "glob failed");
+	                for (u32 m = 0; m < globbuf.gl_pathc; m++) {
+				char *filename = globbuf.gl_pathv[m];
+				struct stat infos;
+        			if (stat(filename, &infos))
+                			err(1, "fstat failed on %s", filename);
+        			u64 size = infos.st_size;		                     
+		                struct dict_t buffer[size / sizeof(struct dict_t)];
+				FILE * f = fopen(filename, "r");
+                                if (f == NULL)
+                                       	err(1, "cannot open %s for reading", filename);
+				u32 check = fread(buffer, sizeof(*buffer), size / sizeof(struct dict_t), f);	
+                                if (ferror(f))
+                                        err(1, "fread failed");
+				if (check != size / sizeof(struct dict_t))
+                			errx(1, "incomplete read %s", filename);
+
+				//printf("Lecture de %s\n", filename);
+
+				#pragma omp parallel for
+				for (u32 j = 0; j < check; j++) 
+				for (u32 k = 0; k < nb_solutions; k++) 
+				                if (buffer[j].hash == solutions[k].solution[kind]) {
+				                       	printf("found hash (list A) %016" PRIx64 " in %s\n", solutions[k].solution[kind], filename);
+				                               preimages[k][kind].counter = buffer[j].preimage.counter;
+				                              	preimages[k][kind].nonce = buffer[j].preimage.nonce;
+							#pragma omp atomic
+							nb_preimages++;
+						}
+					
+					fclose(f);
+				}
+				globfree(&globbuf);
 			}
 		}
 
-		//Charger les dictionnaires
-		int nb_preimages = 0;
-		for (u32 i = 0; i < (1 << k); i++) {
-
-			for (u32 kind = 0; kind < 3; kind++) {
-		                char pattern[255];
-		                char *kind_prefix[3] = {"foo", "bar", "foobar"};
-		                sprintf(pattern, "%s/%03x/%s.*.sorted", dict_dir, i, kind_prefix[kind]);
-		                glob_t globbuf;
-		                if (glob(pattern, 0, NULL, &globbuf))
-		                        err(1, "glob failed");
-		                for (u32 m = 0; m < globbuf.gl_pathc; m++) {
-					char *filename = globbuf.gl_pathv[m];
-					struct stat infos;
-        				if (stat(filename, &infos))
-                				err(1, "fstat failed on %s", filename);
-        				u64 size = infos.st_size;		                     
-		                	struct dict_t buffer[size / sizeof(struct dict_t)];
-					FILE * f = fopen(filename, "r");
-                                	if (f == NULL)
-                                        	err(1, "cannot open %s for reading", filename);
-					u32 check = fread(buffer, sizeof(*buffer), size / sizeof(struct dict_t), f);	
-                                        if (ferror(f))
-                                                err(1, "fread failed");
-					if (check != size / sizeof(struct dict_t))
-                				errx(1, "incomplete read %s", filename);
-
-					//printf("Lecture de %s\n", filename);
-
-					#pragma omp parallel for
-					for (u32 j = 0; j < check; j++) 
-                                                for (u32 k = 0; k < nb_solutions; k++) 
-				                	if (buffer[j].hash == solutions[k].solution[kind]) {
-				                        	printf("found hash (list A) %016" PRIx64 " in %s\n", solutions[k].solution[kind], filename);
-				                                preimages[k][kind].counter = buffer[j].preimage.counter;
-				                               	preimages[k][kind].nonce = buffer[j].preimage.nonce;
-								#pragma omp atomic
-								nb_preimages++;
-				                        }
-					
-					fclose(f);
-					
-
-				}
-
-				globfree(&globbuf);
-			}	
-		}
-
 		if (nb_preimages != 3 * nb_solutions)
-			errx(1, "Charles s'est trompé ! Seulement %d preimages trouvées, %d attendues\n", nb_preimages, 3 * nb_solutions);
+			errx(1, "ERREUR ! Seulement %d preimages trouvées, %d attendues\n", nb_preimages, 3 * nb_solutions);
 
 		char *filename = "preimages.bin";
 		FILE *f_preimages = fopen(filename, "w");
@@ -161,11 +152,7 @@ void check_solutions(char *solutions_filename, u32 k)
 				}
 			}
 		}
-
 		printf("\nBEST = %d bits\n", best);
-			
-
-	
 }
 
 
