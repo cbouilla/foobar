afa6ac3073ff345d5f2b85709dfd810a7af81706
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-04-18 13:00:49 +0200

    improved slicer

--- preprocessing/slicer_test.c
+++ preprocessing/slicer_test.c
@@ -12,6 +12,8 @@
 #include <gmp.h>
 #include "preprocessing.h"
 
+#define VERBOSE 0
+
 u64 * load(const char *filename, u64 *size_)
 {
         struct stat infos;
@@ -44,7 +46,7 @@ struct list_t *active, *reject;
 u32 m;
 
 
-void list_insert(struct list_t *list, struct list_t *item)
+static void list_insert(struct list_t *list, struct list_t *item)
 {
         item->next = list->next;
         item->prev = list;
@@ -52,20 +54,20 @@ void list_insert(struct list_t *list, struct list_t *item)
         item->prev->next = item;        
 }
 
-void list_remove(struct list_t *item)
+static void list_remove(struct list_t *item)
 {
         item->next->prev = item->prev;
         item->prev->next = item->next;
 }
 
-void list_clear(struct list_t *item)
+static void list_clear(struct list_t *item)
 {
         assert(item->x == 0);
         item->next = item;
         item->prev = item;
 }
 
-bool satisfy_equation(u64 x, u64 eq)
+static inline bool satisfy_equation(u64 x, u64 eq)
 {
         return (__builtin_popcountll(x & eq) & 1) == 0;
 }
@@ -74,7 +76,7 @@ void filter_active(u64 eq)
 {
         struct list_t *item = active->next;
         while (item != active) {
-                struct list_t *next = item->next;   /* we have to save this before modifying it */
+                struct list_t *next = item->next;      /* we have to save this before modifying it */
                 if (!satisfy_equation(item->x, eq)) {
                         list_remove(item);
                         list_insert(reject, item);
@@ -228,7 +230,7 @@ void swap_columns(u64 *T, u32 w, u64 j, u64 l)
    There are 64 input rows and m input columns.
    Returns j such that columns [0:j] are echelonized.
    rows [j+1:64] are zero. */
-int echelonize(u64 *T, u32 m, u32 w, int d, u64 *E)
+int echelonize(u64 *T, u32 m, u32 w, int d, u64 *E, bool allow_column_swap)
 {
 	/* E is the change of basis matrix */
 	for (u32 i = 0; i < 64; i++)
@@ -255,12 +257,12 @@ int echelonize(u64 *T, u32 m, u32 w, int d, u64 *E)
 		                        break;
 		                }
 		        }
-		        if (i >= 0)
+		        if (i >= 0 || !allow_column_swap)
 		                break;    /* found pivot */
 
+                        
 			/* pivot not found. This means that the d first columns 
 			   are linearly dependent. We swap the j-th column with the o-th. */
-
 			i32 o;
 			if ((n_random_trials >= 0) && (j + 1 < m)) {
 				o = (j + 1) + (lrand48() % (m - (j + 1)));
@@ -271,9 +273,8 @@ int echelonize(u64 *T, u32 m, u32 w, int d, u64 *E)
 				o = l;
 				l++;
 			}
-			// printf("trying %d <---> %d\n", j, o);
 			swap_columns(T, w, j, o);
-		}
+                }
 		if (i < 0)
 			return j;
                                         
@@ -288,7 +289,6 @@ int echelonize(u64 *T, u32 m, u32 w, int d, u64 *E)
 		for (i32 k = 0; k < 64; k++) {
 			if ((k != j) & ((T[k * w] & mask) != 0)) {
 				E[k] ^= E[j];         /* record the operation */
-				// printf("Doing T[%d] <-- T[%d] + T[%d]\n", k, k, j);
 				for (u32 l = 0; l < w; l++)
 					T[k * w + l] ^= T[j * w + l];
 			}
@@ -309,12 +309,9 @@ int check_rank_defect(const u64 *M, u32 m, u64 *equations, int k) {
        	int d = 64 - k;
        	transpose(M, w, T);
        	
-       	int j = echelonize(T, m, w, d, E);
+       	int j = echelonize(T, m, w, d, E, true);
        	if (j == d)
        		return k;
-
-       	// printf("j = %d\n", j);
-       	// print_matrix(64, w, T);
 
 	/* not enough pivots found: rank defect */
 	for (int r = j; r < d; r++) {
@@ -330,23 +327,161 @@ int check_rank_defect(const u64 *M, u32 m, u64 *equations, int k) {
 	return k;
 }
 
+static u64 myrand() 
+{
+        u64 a = lrand48();
+        u64 b = lrand48();
+        return a + (b << 32);
+}
+
+static u64 naive_gemv(u64 x, const u64 * M)
+{
+        u64 y = 0;
+        for (u32 i = 0; i < 64; i++) {
+                u64 bit = (x >> i) & 1;
+                u64 mask = (u64) (-((i64) bit));
+                y ^= M[i] & mask;
+        }
+        return y;
+}
+
+bool invert(const u64 *M_, u64 *Minv)
+{
+        u64 M[64];
+        for (u64 i = 0; i < 64; i++) {
+                M[i] = M_[i];
+                Minv[i] = 1ull << i;
+        }
+        for (u64 j = 0; j < 64; j++) {
+                /* search pivot */
+                i32 i = -1;
+                u64 mask = 1ull << j;
+                for (i32 k = j; k < 64; k++)
+                        if (((M[k] & mask) != 0)) {
+                                i = k;
+                                break;
+                        }
+                if (i < 0)
+                        return false;
+                swap(M, i, j);
+                swap(Minv, i, j);
+                
+                /* use the pivot to eliminate everything else on the column */
+                for (i32 k = 0; k < 64; k++)
+                        if ((k != j) & ((M[k] & mask) != 0)) {
+                                M[k] ^= M[j];
+                                Minv[k] ^= Minv[j];
+                        }
+        }
+        return true;
+}
+
+
+int slice_it(int l, u64 *equations)
+{
+        int k = 0;
+        while (k < l) {
+                /* copy the active vectors into M, and pad with zeros to reach a multiple of 64 */
+                u32 w = ceil(m / 64.);
+                u32 rows = 64 * w;
+                u64 M[rows];
+                u32 i = 0;
+                for (struct list_t *item = active->next; item != active; item = item->next)
+                        M[i++] = item->x;
+                while (i < rows)
+                        M[i++] = 0x0000000;
+
+                k = check_rank_defect(M, m, equations, k);
+                if (k >= l)
+                        break;
+
+                int d = 64 - k;
+                double R = ((double) d) / m;
+                double expected_w = ceil(m * GV(R));
+           
+                assert(m >= d); /* we should not be worse than naive linear algebra */
+
+                if (VERBOSE)
+                        printf("length=%d, dimension=%d, Rate = %.3f, GV bound: %.0f\n", 
+                                m, d, R, expected_w);
+                else
+                        printf("%d ", m);
+                fflush(stdout);
+
+                /* setup low-weight search */
+                u32 best_weight = m;
+                u64 best_equation = 0;
+                u32 n_iterations = MAX_ISD_ITERATIONS * 150000 / m;
+                
+                if (VERBOSE)
+                        printf("Going for %d iterations\n", n_iterations);
+
+                u64 T[rows];
+                u64 E[64];
+                for (u32 it = 0; it < n_iterations; it++) {
+                        /* this is one iteration of the Lee-Brickell algorithm */
+
+                        /* random permutation of the rows */
+                        for (i32 i = 0; i < d; i++) {
+                                i32 j = i + (lrand48() % (m - i));
+                                swap(M, i, j);
+                        }
+
+                        /* transpose the matrix, in order to access the columns efficiently */
+                        transpose(M, w, T);
+
+                        int j = echelonize(T, m, w, d, E, true);
+                        assert(j == d);
+
+                        /* look for a low-weight row */
+                        for (u32 i = 0; i < 64 - k; i++) {
+                                u32 weight = 0;
+                                for (u32 j = 0; j < w; j++)
+                                        weight += __builtin_popcountll(T[i * w + j]);
+                                if (weight < best_weight) {
+                                        if (VERBOSE)
+                                                printf("\rw = %d (%d iterations%d)", weight, it, i);
+                                        best_weight = weight;
+                                        best_equation = E[i];
+                                }
+                        }
+                        if (best_weight <= expected_w + 1) {
+                                // printf("\nweight small enough; early abort\n");
+                                break;
+                        }
+                }
+                filter_active(best_equation);
+                equations[k++] = best_equation;
+                if (VERBOSE) {
+                        printf("\nBest weight=%d, equation=%" PRIx64 "\n", best_weight, best_equation);
+                        printf("Done an ISD pass. I now have %d equations and %d active vectors\n", k, m);
+                }
+        }
+        assert(k >= l);
+        if (VERBOSE)
+                printf("Finished: I now have %d equations and %d active vectors\n", k, m);
+        else
+                printf("%d (%d)\n", m, k);
+        
+        return k;
+}
 
 int main(int argc, char **argv)
 {
         /* process command-line options */
         struct option longopts[3] = {
-                {"target-dir", required_argument, NULL, 't'},
+                {"output", required_argument, NULL, 't'},
                 {"l", required_argument, NULL, 'l'},
                 {NULL, 0, NULL, 0}
         };
-        char *target_dir = NULL;
+        char *target = NULL;
         char *in_filename = NULL;
         signed char ch;
         i32 l = 0;
         while ((ch = getopt_long(argc, argv, "", longopts, NULL)) != -1) {
                 switch (ch) {
                 case 't':
-                        target_dir = optarg;
+                        target = optarg;
                         break;
                 case 'l':
                         l = atoi(optarg);
@@ -357,8 +492,8 @@ int main(int argc, char **argv)
         }
         if (optind >= argc)
                 errx(1, "missing input file");
-        if (target_dir == NULL)
-                errx(1, "missing --target-dir");
+        if (target == NULL)
+                errx(1, "missing --output");
         in_filename = argv[optind];
 
         u64 n;
@@ -369,6 +504,10 @@ int main(int argc, char **argv)
                 	l = 19;
                 printf("NOTE: Using default l = %d\n", l);
         }
+
+        FILE *f_out = fopen(target, "w");
+        if (f_out == NULL)
+                err(1, "cannot open %s\n", target);
 
         /* setup doubly-linked lists with dummy node */
         struct list_t *items = malloc(n * sizeof(*items));
@@ -393,128 +532,55 @@ int main(int argc, char **argv)
 
         /* slice until the input list is empty */
         while (n > 0) {
-                printf("Starting slice with %d active vectors\n", m);
+                printf("--> ");
                 u64 equations[64];
-                i32 k = 0; /* #equations */
-               
-                while (k < l) {
-			printf("Attacking %d active vectors with ISD\n", m);
-
-			/* copy the active vectors into M, and pad with zeros to reach a multiple of 64 */
-        		u32 w = ceil(m / 64.);
-			u32 rows = 64 * w;
-                	u64 M[rows];
-                	u32 i = 0;
-                	for (struct list_t *item = active->next; item != active; item = item->next)
-                	        M[i++] = item->x;
-                	while (i < rows)
-                	        M[i++] = 0x0000000;
-
-                	k = check_rank_defect(M, m, equations, k);
-                	if (k >= l)
-                		break;
-
-                        int d = 64 - k;
-                        double R = ((double) d) / m;
-                        double expected_w = ceil(m * GV(R));
-           
-			assert(m >= d); /* we should not be worse than naive linear algebra */
-
-                        /* compute expected number of iteration to reach expected weight */
-                        mpz_t a, b, c;
-                        mpz_init(a);
-                        mpz_init(b);
-                        mpz_init(c);
-                        mpz_bin_uiui(a, m, expected_w);
-                        mpz_bin_uiui(b, m - d, expected_w - 1);
-                        mpz_mul_ui(b, b, d);
-                        mpz_tdiv_q(c, a, b);
-                        double expected_its = mpz_get_d(c);
-                        printf("length=%d, dimension=%d, Rate = %.3f, GV bound: %.0f, E[iterations] = %f\n", m, d, R, expected_w, expected_its);
-
-                        /* setup low-weight search */
-                        u32 best_weight = m;
-                        u64 best_equation = 0;
-                        u32 n_iterations = MAX_ISD_ITERATIONS * 150000 / m;
-                        if (3 * expected_its < n_iterations)
-                        	n_iterations = 3 * expected_its;
-                        printf("Going for %d iterations\n", n_iterations);
-
-                        u64 T[rows];
-                        u64 E[64];
-                        for (u32 it = 0; it < n_iterations; it++) {
-                                /* this is one iteration of the Lee-Brickell algorithm */
-
-                                /* random permutation of the rows */
-                                for (i32 i = 0; i < d; i++) {
-                                        i32 j = i + (lrand48() % (m - i));
-                                        swap(M, i, j);
-                                }
-
-                                /* transpose the matrix, in order to access the columns efficiently */
-                                transpose(M, w, T);
-
-				int j = echelonize(T, m, w, d, E);
-				assert(j == d);
-				
-#if 0
-				/* check thruthfullnes of T vs E[i] and M. In principe T = E * M.t ---> T_ij == */
-				for (u32 i = 0; i < 64 - k; i++) {
-					for (u32 j = 0; j < m; j++) {
-						u64 u = j / 64;
-						u64 v = j % 64;
-						u32 a = __builtin_popcountll(M[j] & E[i]) & 1;
-						u32 b = (T[i * w + u] >> v) & 1;
-						if (a != b) {
-							printf("Just after transpose. Problem. i=%d, j=%d.\n", i, j);
-							printf("E[i] = %016" PRIx64 ", M[j] = %016" PRIx64 "\n", E[i], M[j]);
-							printf("T[i] = ");
-							for (int k = 0; k < w; k++)
-								printf("%016" PRIx64 " ", T[i * w + k]);
-							printf("\n");
-							assert(a == b);
-						}
-					}
-				}
-#endif
-
-
-                                /* look for a low-weight row */
-		                for (u32 i = 0; i < 64 - k; i++) {
-		                        u32 weight = 0;
-		                        for (u32 j = 0; j < w; j++)
-		                                weight += __builtin_popcountll(T[i * w + j]);
-		                        if (weight < best_weight) {
-		                                printf("\rw = %d (%d iterations%d)", weight, it, i);
-		                                best_weight = weight;
-		                                best_equation = E[i];
-		                        }
-		                }
-		                if (best_weight <= expected_w + 1) {
-		                	printf("\nweight small enough; early abort\n");
-		                	break;
-		                }
-                        }
-                        filter_active(best_equation);
-                        equations[k++] = best_equation;
-                        printf("\nBest weight=%d, equation=%" PRIx64 "\n", best_weight, best_equation);
-                        
-                        printf("Done an ISD pass. I now have %d equations and %d active vectors\n", k, m);
-                }
-                assert(k >= l);
-                printf("Finished: I now have %d equations and %d active vectors\n", k, m);
-                n -= m;
+                
+                i32 k = slice_it(l, equations);              
                 
                 /* Here, we would need to save the slice to the file !!!
                    Instead, we do nothing at all... */
-                for (i32 i = 0; i < k; i++)
-                        printf("eq[%d] = %016" PRIx64 "\n", i, equations[i]);
+                if (VERBOSE)
+                        for (i32 i = 0; i < k; i++)
+                                printf("eq[%d] = %016" PRIx64 "\n", i, equations[i]);
+
+                /* pad equations to obtain an invertible 64x64 matrix */
+                struct slice_t *slice = malloc(sizeof(*slice) + sizeof(u64) * m);
+                slice->n = m;
+                slice->l = k;
+                bool ok = false;
+                while (!ok) {
+                        u64 T[64];
+                        for (i32 i = 0; i < k; i++)
+                                T[i] = equations[i];
+                        for (u64 i = k; i < 64; i++)
+                                T[i] = myrand();
+                        transpose_64(T, slice->M);
+                        ok = invert(slice->M, slice->Minv);
+                }
+
+                u32 i = 0;
+                for (struct list_t *item = active->next; item != active; item = item->next)
+                        slice->CM[i++] = naive_gemv(item->x, slice->M);
+
+                /* check inversion */
+                for (int i = 0; i < 64; i++)
+                        assert(naive_gemv(slice->M[i], slice->Minv) == 1ull << i);
+
+                /* check zero bits */
+                // for (int i = 0; i < m; i++)
+                //        printf("%016" PRIx64 "\n", slice->CM[i]);
+                        // assert((slice->CM[i] & LEFT_MASK(l)) == 0);
+        
+                /* fwrite(slice) */
+
+                free(slice);
 
                 /* The active (=good) vectors are discarded. The rejected vectors become active again for the next pass. */
                 struct list_t *tmp = active;
                 active = reject;
                 reject = tmp;
                 list_clear(reject);
+                n -= m;
                 m = n;
         }
         exit(EXIT_SUCCESS);
