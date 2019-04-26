630eaa454b6c3451b50485f6a96793df43340726
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-04-26 06:44:05 +0200

    post-processing: cleanup + (old solutions format)

--- postprocessing/post_processing.c
+++ postprocessing/post_processing.c
@@ -2,65 +2,93 @@
 #include <stdlib.h>
 #include <err.h>
 #include <inttypes.h>
-#include <byteswap.h>
 #include <sys/stat.h>
 #include <glob.h>
 #include <getopt.h>
 #include <math.h>
+#include <arpa/inet.h>
+#include <byteswap.h>
+#include <assert.h>
 
-#include "common.h"
 #include "../preprocessing/sha256.h"
 #include "../preprocessing/hasher.h"
 
 //char * hash_di = "/home/mellila/foobar/data/hashes";
 //char * slice_di = "/home/mellila/foobar/data/slice";
-const char * dict_dir = "/home/mellila/foobar/data/dict";
+// const char * dict_dir = "/home/mellila/foobar/data/dict";
 
-bool compute_full_hash(int kind, struct preimage_t *preimage, u32 *hash);
+// bool compute_full_hash(int kind, struct preimage_t *preimage, u32 *hash);
 
-void check_solutions(char *solutions_filename, u32 k)
+struct solution_t {
+	u64 val[3];
+//	u64 task_index[3];
+};
+
+
+bool big_endian()
+{
+	return (htonl(0x47) == 0x47);
+}
+
+u64 * load_file(const char *filename, u64 *size_)
+{
+	struct stat infos;
+	if (stat(filename, &infos))
+		err(1, "fstat failed on %s", filename);
+	u64 size = infos.st_size;
+	assert ((size % 8) == 0);
+	u64 *content = malloc(size);
+	if (content == NULL)
+		err(1, "failed to allocate memory");
+	FILE *f = fopen(filename, "r");
+	if (f == NULL)
+		err(1, "fopen failed (%s)", filename);
+	u64 check = fread(content, 1, size, f);
+	if (check != size)
+		errx(1, "incomplete read %s", filename);
+	fclose(f);
+	*size_ = size / 8;
+	if (!big_endian()) {    /* solution files are big-endian... */
+		for (u32 i = 0; i < size / 8; i++)
+			content[i] = bswap_64(content[i]);
+	}
+	return content;
+}
+
+
+void check_solutions(const char * filename, const char *dict_dir, u32 k)
 {
 	// Charger le fichier de solutions 
-	struct stat infos;
-     	if (stat(solutions_filename, &infos))
-               	err(1, "fstat failed on %s", solutions_filename);
-        u64 size = infos.st_size;
-	u64 nb_solutions = size / sizeof(struct solution_t);			
-	struct solution_t *solutions = malloc( size);
-	FILE *f = fopen(solutions_filename, "r");
-        if (f == NULL)
-        	err(1, "fopen failed (%s)", solutions_filename);
-	u32 check = fread(solutions, sizeof(struct solution_t), nb_solutions, f);
-        if (check != nb_solutions)
-               	errx(1, "incomplete read %s", solutions_filename);
-        fclose(f);
-
-	/* turing est big-endian */
-	if (!big_endian())
-               	for (u32 i = 0; i < nb_solutions; i++) 
-			for (u32 j = 0; j < 3; j++) 
-	                	solutions[i].val[j] = bswap_64(solutions[i].val[j]);
-
+	u64 size = 0;
+	struct solution_t * solutions = (struct solution_t *) load_file(filename, &size);
+	u64 nb_solutions = size * sizeof(u64) / sizeof(struct solution_t);
+	
+	/* that's what we need to fill out */
 	struct preimage_t preimages[nb_solutions][3];
+	u32 origin[nb_solutions][3];
 	for (u32 i = 0; i < nb_solutions; i++) {
                 for (u32 j = 0; j < 3; j++) {
                         preimages[i][j].counter = 0;
                         preimages[i][j].nonce = 0;
+                        origin[i][j] = 0;
 		}
 	}
 
-	// Charger les dictionnaires
-	int nb_preimages = 0;
-	for (u32 i = 0; i < (1 << k); i++) {
+	/* Load the dicts */
+	u32 nb_preimages = 0;
+	for (u32 i = 0; i < (1u << k); i++) {
 		for (u32 kind = 0; kind < 3; kind++) {
 	                char pattern[255];
 	                char *kind_prefix[3] = {"foo", "bar", "foobar"};
-	                sprintf(pattern, "%s/%03x/%s.*.sorted", dict_dir, i, kind_prefix[kind]);
+	                sprintf(pattern, "%s/%03x/%s.*.unsorted", dict_dir, i, kind_prefix[kind]);
 	                glob_t globbuf;
 	                if (glob(pattern, 0, NULL, &globbuf))
 	                        err(1, "glob failed");
+	                
 	                for (u32 m = 0; m < globbuf.gl_pathc; m++) {
 				char *filename = globbuf.gl_pathv[m];
+			
+				/* load dict */
 				struct stat infos;
         			if (stat(filename, &infos))
                 			err(1, "fstat failed on %s", filename);
@@ -75,45 +103,46 @@ void check_solutions(char *solutions_filename, u32 k)
 				if (check != size / sizeof(struct dict_t))
                 			errx(1, "incomplete read %s", filename);
 
-				//printf("Lecture de %s\n", filename);
-
+                		/* checking */
 				#pragma omp parallel for
-				for (u32 j = 0; j < check; j++) 
-				for (u32 k = 0; k < nb_solutions; k++) 
+				for (u32 j = 0; j < check; j++) {
+					for (u32 k = 0; k < nb_solutions; k++) {
 				                if (buffer[j].hash == solutions[k].val[kind]) {
-				                       	printf("found hash (list A) %016" PRIx64 " in %s\n", solutions[k].val[kind], filename);
-				                               preimages[k][kind].counter = buffer[j].preimage.counter;
-				                              	preimages[k][kind].nonce = buffer[j].preimage.nonce;
+				                       	printf("found hash %016" PRIx64 " in %s\n", solutions[k].val[kind], filename);
+							preimages[k][kind].counter = buffer[j].preimage.counter;
+							preimages[k][kind].nonce = buffer[j].preimage.nonce;
+							origin[k][kind] = i;
 							#pragma omp atomic
 							nb_preimages++;
 						}
-					
-					fclose(f);
+					}
 				}
-				globfree(&globbuf);
+				fclose(f);
 			}
+			globfree(&globbuf);
 		}
+	}
 
-		if (nb_preimages != 3 * nb_solutions)
-			errx(1, "ERREUR ! Seulement %d preimages trouvées, %d attendues\n", nb_preimages, 3 * nb_solutions);
+	if (nb_preimages != 3 * nb_solutions)
+		errx(1, "ERREUR ! Seulement %d preimages trouvées, %ld attendues\n", nb_preimages, 3 * nb_solutions);
 
-		char *filename = "preimages.bin";
-		FILE *f_preimages = fopen(filename, "w");
-       		if (f_preimages == NULL)
-                	err(1, "fopen failed (%s)", filename);
-		u32 ch = fwrite(preimages,sizeof(struct preimage_t), nb_solutions * 3, f_preimages);
-		if (ch != nb_solutions * 3)
-	                errx(1, "incomplete write %s", filename);
-        	fclose(f_preimages);
+	char *pre_filename = "preimages.bin";
+	FILE *f_preimages = fopen(pre_filename, "w");
+       	if (f_preimages == NULL)
+               	err(1, "fopen failed (%s)", filename);
+	u32 ch = fwrite(preimages,sizeof(struct preimage_t), nb_solutions * 3, f_preimages);
+	if (ch != nb_solutions * 3)
+	               errx(1, "incomplete write %s", filename);
+        fclose(f_preimages);
 
-		printf("Le nombre de solutions : %d\n", nb_solutions);
+	printf("Le nombre de solutions : %ld\n", nb_solutions);
 		/*for (int k = 0; k < nb_solutions; k++) {
 			 printf("%016" PRIx64 " ^ %016" PRIx64 " ^ %016" PRIx64 " == 0\n",
                                                 solutions[k].x,
                                                 solutions[k].y,
                                                 solutions[k].z);
 		}*/
-		printf("Le nombre de préimages trouvées : %d\n", nb_preimages);
+	printf("Le nombre de préimages trouvées : %d\n", nb_preimages);
 		/*for (u32 k = 0; k < nb_solutions; k++) {
 			for (u32 j = 0; j < 3; j++) {
 				printf("PREIMAGE[%d][%d].counter : %d\n", k, j, preimages[k][j].counter);
@@ -121,38 +150,40 @@ void check_solutions(char *solutions_filename, u32 k)
 			}
 		}*/
 		
-		u32 best = 0;
-		struct preimage_t best_preimages[3];
-		for (u32 kind = 0; kind < 3; kind++) {
-			best_preimages[kind].counter = preimages[0][kind].counter;
-			best_preimages[kind].nonce = preimages[0][kind].nonce;
-		}
+	u32 best = 0;
+	struct preimage_t best_preimages[3];
+	for (u32 kind = 0; kind < 3; kind++) {
+		best_preimages[kind].counter = preimages[0][kind].counter;
+		best_preimages[kind].nonce = preimages[0][kind].nonce;
+	}
 		
-		for (u32 i = 0; i < nb_solutions; i++) {
-			u32 sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-			for (u32 kind = 0; kind < 3; kind++) {
-				u32 hash[8];
-				bool valid = compute_full_hash(kind, &preimages[i][kind], hash);
-				if (!valid)
-					warnx("bizarre, invalid preimage");
-				for (u32 p = 0; p < 8; p++)
-					sum[p] ^= hash[p];
-			}
-			printf("SUM = ");
+	for (u32 i = 0; i < nb_solutions; i++) {
+		u32 sum[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+		for (u32 kind = 0; kind < 3; kind++) {
+			u32 hash[8];
+			bool valid = compute_full_hash(kind, &preimages[i][kind], hash);
+			if (!valid)
+				warnx("bizarre, invalid preimage");
 			for (u32 p = 0; p < 8; p++)
-				printf("%08x ", sum[7 - p]);
-			printf("\n");
-			u32 bits = 128 - ceil(log2(sum[4]));
-			printf(" --- %d bits\n", bits);
-			if (bits > best) {
-				best = bits;
-				for (u32 kind = 0; kind < 3; kind++) {
-					best_preimages[kind].counter = preimages[i][kind].counter;
-				        best_preimages[kind].nonce = preimages[i][kind].nonce;
-				}
+				sum[p] ^= hash[p];
+		}
+		printf("[%04x ; %04x ; %04x] ", origin[i][0], origin[i][1], origin[i][2]);
+		printf("SUM = ");
+		for (u32 p = 0; p < 8; p++)
+			printf("%08x ", sum[7 - p]);
+		u32 bits = 128 - ceil(log2(sum[4]));
+		printf(" --- %d bits\n", bits);
+		if (bits > best) {
+			best = bits;
+			for (u32 kind = 0; kind < 3; kind++) {
+				best_preimages[kind].counter = preimages[i][kind].counter;
+			        best_preimages[kind].nonce = preimages[i][kind].nonce;
 			}
 		}
-		printf("\nBEST = %d bits\n", best);
+	}
+	printf("\nBEST = %d bits\n", best);
+	for (u32 kind = 0; kind < 3; kind++)
+		printf("%016" PRIx64 " / %08x\n", best_preimages[kind].counter, best_preimages[kind].nonce);
 }
 
 
@@ -174,8 +205,9 @@ int main(int argc, char *argv[])
                         errx(1, "Unknown option\n");
                 }
         }
+	
 	printf("Début, k=%d\n", k);
-	check_solutions("/home/mellila/solutions.bin", k);
+	check_solutions("../solutions/solutions.bin", "../data/dict", k);
 }
 
 
