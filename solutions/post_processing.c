e28c086e1b8ef2f312ad2401b79adbe746d0d0fd
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-09-13 22:02:39 +0200

    fix

--- solutions/post_processing.c
+++ solutions/post_processing.c
@@ -72,8 +72,8 @@ u32 print_result(struct preimage_t (* preimages)[3], u32 (*origin)[3], u32 i)
 
 	u32 hash[8];
 	compute_middle_hash(2, &preimages[i][2], hash);
-	bits = 33 + (32 - ceil(log2(hash[7])));
-	printf(" --- clamped to %d bits [%08x %08x]\n", bits, hash[7], hash[8]);
+	bits = 33 + ((hash[6] == 0) ? 32 : 32 - ceil(log2(hash[6])));
+	printf(" --- clamped to %d bits [%08x %08x]\n", bits, hash[6], hash[7]);
 	return bits;
 }
 
