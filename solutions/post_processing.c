597addedc587bff536b61fbf1e78af598f70721f
Charles Bouillaguet <charles.bouillaguet@univ-lille1.fr>
2019-09-16 16:56:30 +0200

    better solution postproc

--- solutions/post_processing.c
+++ solutions/post_processing.c
@@ -64,15 +64,19 @@ u32 print_result(struct preimage_t (* preimages)[3], u32 (*origin)[3], u32 i)
 	}
 
 	printf("[%04x ; %04x ; %04x] ", origin[i][0], origin[i][1], origin[i][2]);
-	printf("SUM = ");
-	for (u32 p = 0; p < 8; p++)
-		printf("%08x ", sum[7 - p]);
+	// printf("SUM = ");
+	// for (u32 p = 0; p < 8; p++)
+	// 	printf("%08x ", sum[7 - p]);
 	u32 bits = (sum[4] == 0) ? 128 : 128 - ceil(log2(sum[4]));
-	printf(" --- %d bits", bits);
+	// printf(" --- %d bits", bits);
 
 	for (u32 kind = 0; kind < 3; kind++) {
-		u32 bits = 32 + ((hash[kind][6] == 0) ? 32 : 32 - ceil(log2(hash[kind][6])));
-		printf(" --- clamped to %d bits [%08x %08x]", bits, hash[kind][6], hash[kind][7]);
+		for (u32 p = 0; p < 8; p++)
+		 	printf("%08x ", hash[kind][p]);
+		if (kind != 2) 
+			printf(" | ");
+		//u32 bits = 32 + ((hash[kind][6] == 0) ? 32 : 32 - ceil(log2(hash[kind][6])));
+		//printf(" --- clamped to %d bits [%08x %08x]", bits, hash[kind][6], hash[kind][7]);
 	}
 	printf("\n");
 	return bits;
